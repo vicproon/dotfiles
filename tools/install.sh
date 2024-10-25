ebcccb1fda57ac7ca83a0e64c48528394aae7256
Viktor Prun <v.prun@cognitivepilot.com>
2024-10-25 14:40:26 +0300

    install for svn vimwraps

---
+++ tools/install.sh
@@ -0,0 +1,10 @@
+# /bin/bash
+
+# echo $PWD/vimdiffwrap.sh
+
+sudo ln -sf $PWD/vimdiffwrap.sh /usr/local/bin/vimdiffwrap
+sudo ln -sf $PWD/vimmergewrap.sh /usr/local/bin/vimmergewrap
+
+ls -al /usr/local/bin/vim*wrap
+echo DONE
+
